5f440fc0519a14ec43b7cc059b08d9cd79591750
Antonio Pérez <apema2079@gmail.com>
2021-11-10 15:52:06 -0600

    Receive cart object, and count items and send to BubbleAlert element

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import BubbleAlert from "./BubbleAlert";
 
 const styles = {
     cart: {
@@ -8,15 +9,27 @@ const styles = {
         padding: '15px',
         borderRadius: '15px',
         cursor: 'pointer',
+    },
+    bubble: {
+        position: 'relative',
+        left: 12,
+        top: 20,
     }
 }
 
 class Cart extends Component {
     render() {
+        const { cart } = this.props
+        const quantity = cart.reduce((total, item) => total + item.quantity, 0)
         return (
-            <button style={ styles.cart }>
-                Carrito
-            </button>
+            <div>
+                <span style={ styles.bubble }>
+                    { quantity !== 0 ? <BubbleAlert value={ quantity }/> : null}
+                </span>
+                <button style={ styles.cart }>
+                    Carrito
+                </button>
+            </div>
         )
     }   
 }
