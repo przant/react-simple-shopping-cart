bc13de2aab9de3e50985d3542bf7c68f9b66302d
Antonio Pérez <apema2079@gmail.com>
2021-11-10 12:23:56 -0600

    Apply new components to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import { Component } from 'react'
 import Products from './components/Products'
+import Layout from './components/Layout'
 import './App.css'
 
 class App extends Component {
   state = {
     products: [
-      { name: 'Tomates', price: 1500, img: '/products/tomate.jpg' },
-      { name: 'Chicharos', price: 2500, img: '/products/chicharos.jpg'},
-      { name: 'Lechuga', price:  500, img: '/products/lechuga.jpg'},
+      { name: 'Tomates', price: 25, img: '/products/tomate.jpg' },
+      { name: 'Chicharos', price: 30, img: '/products/chicharos.jpg'},
+      { name: 'Lechuga', price:  15, img: '/products/lechuga.jpg'},
     ]
   }
   render() {
     return (
       <div>
-        <Products
-          addToShoppingCart={ () => console.log('Do nothing') }
-          products={ this.state.products }
-        >
-
-        </Products>
+        <Layout>
+          <Products
+            addToShoppingCart={ () => console.log('Do nothing') }
+            products={ this.state.products }
+          />
+        </Layout>
       </div>
     )
   }
